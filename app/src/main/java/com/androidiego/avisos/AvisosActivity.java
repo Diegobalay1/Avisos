cc08c6ed04395a7d8ba9ccfbfe486291fc111f42
Diego Luis <dlolhd@gmail.com>
2019-08-21 16:33:22 +0200

    Añadidos items nuevo aviso y salir al menú

--- app/src/main/java/com/androidiego/avisos/AvisosActivity.java
+++ app/src/main/java/com/androidiego/avisos/AvisosActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,7 +47,7 @@ public class AvisosActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_avisos, menu);
         return true;
     }
 
@@ -57,11 +58,18 @@ public class AvisosActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_nuevo:
+                // crear nuevo aviso
+                Log.d(getLocalClassName(), "crear nuevo Aviso");
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+            default:
+                return false;
         }
 
-        return super.onOptionsItemSelected(item);
+        //return super.onOptionsItemSelected(item);
     }
 }
